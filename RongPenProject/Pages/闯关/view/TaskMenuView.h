338920dccd92cbfe1c557caa7f5f8af9ba1a2334
陈志刚 <chenzg@misbio.com>
2020-09-19 14:48:45 +0800

    添加tabbar图片

---
+++ RongPenProject/Pages/闯关/view/TaskMenuView.h
@@ -0,0 +1,16 @@
+//
+//  TaskMenuView.h
+//  RongPenProject
+//
+//  Created by 小米手机 on 2020/9/19.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TaskMenuView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RongPenProject/Pages/闯关/view/TaskMenuView.m
@@ -0,0 +1,20 @@
+//
+//  TaskMenuView.m
+//  RongPenProject
+//
+//  Created by 小米手机 on 2020/9/19.
+//
+
+#import "TaskMenuView.h"
+
+@implementation TaskMenuView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
