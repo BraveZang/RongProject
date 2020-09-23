7fe677f341acf01ef1d31f88bf494134cae28823
zanghui <843035013@qq.com>
2020-09-23 10:46:28 +0800

    no message

--- RongPenProject/Pages/闯关/闯关首页/view/RightSlidingMenuViewCell.m
+++ RongPenProject/Pages/闯关/闯关首页/view/RightSlidingMenuViewCell.m
@@ -16,8 +16,6 @@
         float viewH=FitRealValue(60);
         float viewW=ScreenWidth-FitRealValue(230);
         
-//        self.selectionStyle = UITableViewCellSelectionStyleNone;
-//
         self.yellowView = [[UIView alloc] initWithFrame:CGRectMake(LeftMargin, FitRealValue(18), 5, (viewH-FitRealValue(36)))];
         self.yellowView.backgroundColor = [MTool colorWithHexString:@"#FF5448"];
         self.yellowView.hidden=YES;
@@ -26,7 +24,6 @@
         self.name = [[UILabel alloc] initWithFrame:CGRectMake(self.yellowView.right+10,0,viewW-self.yellowView.right,viewH)];
         self.name.font = [UIFont systemFontOfSize:14];
         self.name.textColor =[MTool colorWithHexString:@"#282828"];
-//        self.name.highlightedTextColor = [MTool colorWithHexString:@"#FF6E1D"];
         [self.contentView addSubview:self.name];
         
     }
