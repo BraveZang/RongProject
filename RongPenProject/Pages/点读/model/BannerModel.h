62255d9fcbf66f95b10cd6ff31c177885f7bb437
zanghui <843035013@qq.com>
2020-10-24 21:49:39 +0800

    no message

---
+++ RongPenProject/Pages/点读/model/BannerModel.h
@@ -0,0 +1,16 @@
+//
+//  BannerModel.h
+//  RongPenProject
+//
+//  Created by 路面机械网  on 2020/10/24.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BannerModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RongPenProject/Pages/点读/model/BannerModel.m
@@ -0,0 +1,12 @@
+//
+//  BannerModel.m
+//  RongPenProject
+//
+//  Created by 路面机械网  on 2020/10/24.
+//
+
+#import "BannerModel.h"
+
+@implementation BannerModel
+
+@end
