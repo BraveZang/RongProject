293f7395241412d3db5eb2e20f8b2d95472e35a7
陈志刚 <18701607782@163.com>
2020-10-25 21:50:49 +0800

    no message

---
+++ RongPenProject/Pages/点读/model/VoiceModel.h
@@ -0,0 +1,16 @@
+//
+//  VoiceModel.h
+//  RongPenProject
+//
+//  Created by czg on 2020/10/25.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface VoiceModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RongPenProject/Pages/点读/model/VoiceModel.m
@@ -0,0 +1,12 @@
+//
+//  VoiceModel.m
+//  RongPenProject
+//
+//  Created by czg on 2020/10/25.
+//
+
+#import "VoiceModel.h"
+
+@implementation VoiceModel
+
+@end
